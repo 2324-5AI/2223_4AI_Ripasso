49eec77e6e53e66ec0499e1bb858e631373acdd1
Daniele Fea <daniele.fea@vallauri.edu>
2023-03-15 13:22:23 +0100

    Introdotto l'evento keyup e aggiunto un primo controllo delle risposte

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.onload = init;
+var domande = [];
 
 
 //window.location.href -> info sull'indirizzo attuale del sito e server
@@ -9,7 +10,7 @@ function init(){
     //Aspettiamo la risposta
     ris.then(async function(dati){
         //Leggiamo i dati della risposta e li convertiamo in json => chiamata asincrona
-        let domande = await dati.json();
+        domande = await dati.json();
         let div = document.getElementById("divDomande");
 
         for(let j in domande){
@@ -27,9 +28,27 @@ function init(){
         }
     });
 
-    document.getElementById("btnControlla").addEventListener("click", controlla);
+    document.getElementById("btnControlla").addEventListener("click", controlla.bind(document.getElementById("btnControlla")));
 }
 
 function controlla(){
+    console.log(this);
+    let risposte = document.querySelectorAll("input:checked");
+    if(risposte.length == domande.length){
+        let contErrate = 0;
+        for(let risposta of risposte){
+            let prova = domande[risposta.name].risp[risposta.value];
+            if(!domande[risposta.name].risp[risposta.value].corretta){
+                contErrate++;
+            }
+        }
+        alert("Hai sbagliato "+ contErrate+" risposte");
 
+    }else{
+        alert("Attenzione, non hai risposto a tutte le domande!");
+    }
+}
+
+function premuto(evento){
+    console.log(evento);
 }
